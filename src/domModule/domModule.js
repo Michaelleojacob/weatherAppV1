7eb6114c8d47f0161bbc2fece35e82be63bb8d59
Michael <michaelleojacob@gmail.com>
2021-10-30 11:57:35 -0700

    added stuff to dommodule

--- src/domModule/domModule.js
+++ src/domModule/domModule.js
@@ -2,6 +2,13 @@ const domObj = {
   container: document.querySelector('#container'),
   form: document.querySelector('#form'),
   input: document.querySelector('#input'),
-  card: document.querySelector('#card'),
+  name: document.querySelector('#name'),
+  status: document.querySelector('#status'),
+  weather: document.querySelector('#weather'),
+  feelsLike: document.querySelector('#feelsLike'),
+  high: document.querySelector('#high'),
+  low: document.querySelector('#low'),
+  wind: document.querySelector('#wind'),
+  humidity: document.querySelector('#humidity'),
 };
 export default domObj;
